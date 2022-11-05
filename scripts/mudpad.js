a81b6094f7ef0d6d8261d1acbe5d3cd3a225b350
gesslar <karahd@gmail.com>
2022-11-04 20:51:15 -0400

    updates

--- scripts/mudpad.js
+++ scripts/mudpad.js
@@ -3,11 +3,16 @@ let editor
 function initializeEditor() {
     editor = ace.edit("editor")
     editor.setTheme("ace/theme/cobalt");
-    editor.session.setMode("ace/mode/text");
     editor.setOption("wrap", 80)
     editor.setFontSize(16)
     const session = editor.getSession()
+    session.setMode("ace/mode/text")
     session.setOption("indentedSoftWrap", false)
     session.setUseWrapMode(true)
 }
 
+function wrapIt() {
+    let text = editor.getSession().getValue()
+    text = wrapify(text, 80, 0)
+    editor.getSession().setValue(text)
+}
